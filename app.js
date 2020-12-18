5674b3a8112462c27fcb1cb6e94bbfb944d3ec57
qml <you@email.com>
2020-12-18 16:02:57 +0800

    refactoryed, search pane and promise all are still undone

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ function searchViaStreetName(streetName){
       Promise.reject(response.statusText);
     }
   })
-  .then(data =>{
+  .then(data => {
     console.log(data.streets);
-    data.streets.forEach(item=>createList(item));
+    data.streets.forEach(item => createList(item));
   })
   .catch(error => {
     alert(`Error: "${error}"`);
@@ -26,10 +26,12 @@ function createList(objectArg){
   streets.insertAdjacentHTML('beforeend',`<a href="#" data-street-key="${objectArg.key}" name="${objectArg.name}">${objectArg.name}</a>`)
 }
 
-form.addEventListener('submit',()=>{
-  if(searchInput.value!==""){
-searchViaStreetName(searchInput.value);
-  }
+form.addEventListener('submit',() => {
+  if(searchInput.value !== ""){
+    console.log(searchInput.value);
+    streets.innerHTML="";
+    searchViaStreetName(searchInput.value);
+  }else{alert("You input is invalid")};
 })
 
 // the main table part
@@ -38,6 +40,7 @@ const detailInformation=[];
 
 const titlebar = document.querySelector("#street-name")
 const tableBody = document.querySelector("tbody")
+
 
 function searchViaStreetKey(streetKey){
   fetch(`https://api.winnipegtransit.com/v3/stops.json?api-key=oavqfxDk4stbDI8v0v5f&street=${streetKey}`)
@@ -50,16 +53,18 @@ function searchViaStreetKey(streetKey){
   })
   .then(data =>{
     console.log(data);
-    data.stops.forEach(item => getStopInformation(item));
-    console.log(stopInformation);
+    data.stops.forEach(item => stopInformation.push(item.key));
     return stopInformation;
   }).then(data=>{
     console.log(data);
-    data.forEach(item=>{
-      addRouteInformation(item.stop_key);
+    data.forEach(item => {
+      addRouteInformation(item);
     });
     return detailInformation;
-  }).then(data=>{console.log(data)})
+  }).then(data => {
+    console.log(data); 
+    createTable(data);
+  })
   .catch(error => {
     alert(`Error: "${error}"`);
   })
@@ -77,11 +82,9 @@ function createTable(arrayArg){
     </tr>`
     tableBody.prepend(tr);
   })
-
 }
 
 function addRouteInformation(stop_key){
-
   fetch(`https://api.winnipegtransit.com/v3/stops/${stop_key}/schedule.json?api-key=oavqfxDk4stbDI8v0v5f`)
   .then(response => {
     if (response.status == 200) {
@@ -91,27 +94,32 @@ function addRouteInformation(stop_key){
     }
   })
   .then(data =>{
-    console.log(data);
-    const k = data['stop-schedule']['stop'].key;
-    const n = data['stop-schedule']['stop'].name;
-    const c = data['stop-schedule']['stop']["cross-street"].name;
-    const d = data['stop-schedule']['stop'].direction;
+    if(data['stop-schedule']['route-schedules'].length!==0){
+      console.log(data);
+      const k = data['stop-schedule']['stop'].key;
+      const n = data['stop-schedule']['stop'].name;
+      const c = data['stop-schedule']['stop']["cross-street"].name;
+      const d = data['stop-schedule']['stop'].direction;
 
-    data['stop-schedule']['route-schedules'].forEach(item => {
-      console.log(item.route.key);
-      const iterationNumber = item['scheduled-stops'].length > 2 ? 2 : item['scheduled-stops'].length;
-      for(let i=0;i<iterationNumber;i++){
-        const tableData = {};
-        tableData.bus_number = item['scheduled-stops'][i].bus.key;
-        tableData.arrive_time = item['scheduled-stops'][i].times.arrival.scheduled || item['scheduled-stops'][i].times.departure.scheduled;
-        tableData.stop_key = k;
-        tableData.stop_name = n;
-        tableData.cross_street = c;
-        tableData.direction = d;
-        tableData.route_number = item.route.key;
-        detailInformation.push(tableData);
-      }
-    });
+      data['stop-schedule']['route-schedules'].forEach(item => {
+        console.log(item.route.key);
+        let iterationNumber = 2;
+        if(item['scheduled-stops'].length<2){
+          iterationNumber = item['scheduled-stops'].length
+        };
+        for(let i=0;i<iterationNumber;i++){
+          const tableData = {};
+          tableData.bus_number = item['scheduled-stops'][i].bus.key;
+          tableData.arrive_time = item['scheduled-stops'][i].times.arrival.scheduled || item['scheduled-stops'][i].times.departure.scheduled;
+          tableData.stop_key = k;
+          tableData.stop_name = n;
+          tableData.cross_street = c;
+          tableData.direction = d;
+          tableData.route_number = item.route.key;
+          detailInformation.push(tableData);
+        }
+      });
+    }
   })
   .catch(error => {
     alert(`Error: "${error}"`);
@@ -120,22 +128,21 @@ function addRouteInformation(stop_key){
 
 
 document.addEventListener('click',(event)=>{
-  if (event.target.nodeName==="A") {
+  if (event.target.nodeName === "A") {
     console.log("yes, you hit it");
     console.log(event.target.getAttribute("data-street-key"));
+    titlebar.innerHTML ="";
+    tableBody.innerHTML="";
     let k = event.target.getAttribute("data-street-key");
+    searchViaStreetKey(k);
+    
     let n = event.target.getAttribute("name");
     titlebar.innerHTML = `Displaying results for ${n}`;
-    searchViaStreetKey(k);
-    createTable(detailInformation);
+    
+
   }
 })
 
-function getStopInformation(objectArg){
-const tableData = {};
-tableData.stop_key = objectArg.key;
-stopInformation.push(tableData);
-}
 
 
 
